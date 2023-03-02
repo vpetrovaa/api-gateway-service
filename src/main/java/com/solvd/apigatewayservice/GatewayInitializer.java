cb184fd8706a313ca3a5343db8956a0a14f19a44
vpetrovaa <vpetrova.laba@solvd.com>
2023-03-02 12:53:34 +0300

    Project structure was created

---
+++ src/main/java/com/solvd/apigatewayservice/GatewayInitializer.java
@@ -0,0 +1,13 @@
+package com.solvd.apigatewayservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatewayInitializer {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayInitializer.class, args);
+	}
+
+}
